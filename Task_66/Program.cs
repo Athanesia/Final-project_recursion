5957dfcdc90c22ca35df787eac39484f1510b08e
Anna <zhigalovanna.28@gmail.com>
2022-12-23 14:55:16 +0300

    Add gap between N and M

---
+++ Task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int NaturalNumbers(int begin, int end)
+{
+    if (end == begin) return begin;
+    return end + NaturalNumbers(begin, end - 1);
+}
+
+
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m >= n) Console.Write("N должно быть больше M");
+else Console.WriteLine(NaturalNumbers(m, n));
